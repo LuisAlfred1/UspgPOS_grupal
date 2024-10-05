cfee965f2062cf2a9133e781d2b5e07e038e799e
asus <asus@DESKTOP-EOF8MPJ>
2024-10-04 22:09:14 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UspgPOS.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public AuthController() { }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
